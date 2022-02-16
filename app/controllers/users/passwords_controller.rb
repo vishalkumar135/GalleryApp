21a5f7785e58a48270d202d636a17f9c12c26a19
vishal_kumar135 <kumarasvishal@gmail.com>
2022-02-16 09:05:09 +0530

    first

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,6 +1,6 @@
 # # frozen_string_literal: true
 
-# class Users::PasswordsController < Devise::PasswordsController
+ class Users::PasswordsController < Devise::PasswordsController
 #   # GET /resource/password/new
 #    def new
 #      super
@@ -32,4 +32,4 @@
 #    def after_sending_reset_password_instructions_path_for(resource_name)
 #      super(resource_name)
 #    end
-# end
+ end
