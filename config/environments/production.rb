ae483819ef2821e59661a2f301f2d7119c42c6e3
vishal_kumar135 <kumarasvishal@gmail.com>
2022-02-21 17:51:44 +0530

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,10 +117,10 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-  config.action_mailer.default_url_options = { host: 'https://vishal-gallery-app.herokuapp.com/'}
-  # config.action_mailer.perform_deliveries = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
+  config.action_mailer.default_url_options = { host:'https://vishal-gallery-app.herokuapp.com'}
+   config.action_mailer.perform_deliveries = true
+   config.action_mailer.delivery_method = :smtp
+   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'gmail.com',
