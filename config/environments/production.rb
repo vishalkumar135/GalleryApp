dcef2c43ddb30dd590eecbc93edae85ca2204414
vishal_kumar135 <kumarasvishal@gmail.com>
2022-02-21 18:18:05 +0530

    first

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,19 +119,14 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.default_url_options = { host: 'https://vishal-gallery-app.herokuapp.com' }
    
-   config.action_mailer.perform_deliveries = true
-   config.action_mailer.delivery_method = :smtp
-   config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    # domain:               'gmail.com',
-    user_name:            'kumarasvishal@gmail.com',
-    password:             'Vishal18352@',
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 
-  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :user_name => '8a1a882c07a350',
+    :password => 'ef14194b17d2e9',
+    :address => 'smtp.mailtrap.io',
+    :domain => 'smtp.mailtrap.io',
+    :port => '2525',
+    :authentication => :cram_md5
   }
 
   config.active_storage.replace_on_assign_to_many = false
